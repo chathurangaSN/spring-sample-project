7da57831cc1112ab8c26a353d5462db4d8cb31f4
chathuranga nanayakkara <sachithmaths@gmail.com>
2019-05-31 16:47:09 +0530

    discovery service added

--- employee-service/src/main/java/com/example/boot/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/boot/EmployeeServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
